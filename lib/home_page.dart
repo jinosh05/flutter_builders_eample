f719db2a9cc4a32ec7997d54a80c639a95a62307
Jinosh05 <jinoshprabhuraj5@gmail.com>
2024-07-21 23:06:57 +0530

    HomePage Linked with other Pages

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,17 @@
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
+import "package:flutter_builders_eample/builders/bloc_build_when/bloc_build_when_screen.dart";
+import "package:flutter_builders_eample/builders/bloc_with_async/bloc_with_async.dart";
+import "package:flutter_builders_eample/builders/bloc_with_vlb/bloc_with_vlb.dart";
+import "package:flutter_builders_eample/builders/builders.dart";
+import "package:flutter_builders_eample/builders/cubit_build_when/cubit_build_when_screen.dart";
+import "package:flutter_builders_eample/builders/cubit_with_vlb/cubit_with_vlb.dart";
+import "package:flutter_builders_eample/builders/page_memory_bloc/bloc/page_memory_bloc.dart";
+import "package:flutter_builders_eample/builders/page_memory_bloc/page_memory_bloc_screen.dart";
+import "package:flutter_builders_eample/builders/services_with_vlb/service_with_vlb.dart";
+import "package:flutter_builders_eample/builders/universal_bloc/universal_bloc_screen.dart";
+import "package:flutter_builders_eample/builders/value_builders.dart";
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -7,14 +19,30 @@ class HomePage extends StatelessWidget {
   final List<String> topics = <String>[
     "Builder",
     "Value Listenable Builder",
-    "Value Listenable Builder with Bloc / Cubit",
+    "Value Listenable Builder with Bloc",
+    "Value Listenable Builder with Cubit",
     "Value Listenable Builder with Services",
     "Bloc Builders Universal memory",
     "Bloc Builders Page Memory",
     "Bloc Builders with Async Function",
-    "Bloc Builders with Value Listenable Builders",
-    "Cubit and Bloc Builders",
-    "Cubit and Value Listenable Builders",
+    "Cubit Builders Build When",
+    "Bloc Builders Build When",
+  ];
+
+  final List<Widget> pages = <Widget>[
+    const BuildersPage(),
+    const VLBuildersPage(),
+    const BlocWithVlb(),
+    const CubitWithVlb(),
+    const ServiceWithVlb(),
+    const UniversalBlocScreen(),
+    BlocProvider<PageMemoryBloc>(
+      create: (final BuildContext context) => PageMemoryBloc(),
+      child: const PageMemoryScreen(),
+    ),
+    const BlocWithAsync(),
+    const CubitBuildWhenScreen(),
+    const BlocBuildWhenScreen(),
   ];
 
   @override
@@ -29,6 +57,9 @@ class HomePage extends StatelessWidget {
           itemCount: topics.length,
           itemBuilder: (final BuildContext context, final int index) =>
               ListTile(
+            onTap: () async {
+              await push(context, pages[index]);
+            },
             trailing: const Icon(
               Icons.arrow_forward_ios_outlined,
               size: 22,
@@ -46,6 +77,17 @@ class HomePage extends StatelessWidget {
   @override
   void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(IterableProperty<String>("topics", topics));
+    properties
+      ..add(IterableProperty<String>("topics", topics))
+      ..add(IterableProperty<Widget>("pages", pages));
+  }
+
+  static Future<void> push(
+    final BuildContext context,
+    final Widget page,
+  ) async {
+    await Navigator.of(context).push(
+      MaterialPageRoute<Widget>(builder: (final BuildContext context) => page),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:flutter_builders_eample/builders/bloc_build_when/bloc/build_when_bloc.dart";
-import "package:flutter_builders_eample/builders/bloc_build_when/bloc_build_when_screen.dart";
 import "package:flutter_builders_eample/builders/bloc_with_async/bloc/async_bloc.dart";
-import "package:flutter_builders_eample/builders/bloc_with_async/bloc_with_async.dart";
 import "package:flutter_builders_eample/builders/bloc_with_vlb/bloc/vlb_bloc.dart";
-import "package:flutter_builders_eample/builders/bloc_with_vlb/bloc_with_vlb.dart";
-import "package:flutter_builders_eample/builders/builders.dart";
 import "package:flutter_builders_eample/builders/cubit_build_when/cubit/build_when_cubit.dart";
-import "package:flutter_builders_eample/builders/cubit_build_when/cubit_build_when_screen.dart";
 import "package:flutter_builders_eample/builders/cubit_builders/cubit/cubit_base_cubit.dart";
-import "package:flutter_builders_eample/builders/cubit_builders/cubit_builders.dart";
 import "package:flutter_builders_eample/builders/cubit_with_vlb/cubit/cubit1_cubit.dart";
-import "package:flutter_builders_eample/builders/cubit_with_vlb/cubit_with_vlb.dart";
-import "package:flutter_builders_eample/builders/page_memory_bloc/bloc/page_memory_bloc.dart";
-import "package:flutter_builders_eample/builders/page_memory_bloc/page_memory_bloc_screen.dart";
-import "package:flutter_builders_eample/builders/services_with_vlb/service_with_vlb.dart";
 import "package:flutter_builders_eample/builders/universal_bloc/bloc/universal_bloc.dart";
-import "package:flutter_builders_eample/builders/universal_bloc/universal_bloc_screen.dart";
-import "package:flutter_builders_eample/builders/value_builders.dart";
 import "package:flutter_builders_eample/home_page.dart";
 import "package:nested/nested.dart";
 
@@ -61,25 +49,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(final BuildContext context) => MaterialApp(
         theme: ThemeData.dark(),
-        home: Builder(
-          builder: (final BuildContext context) {
-            return const CubitBuildWhenScreen();
-            // ignore: dead_code
-            return const BlocWithVlb();
-            return const CubitWithVlb();
-            return const CubitBuildersEg();
-            return const BuildersPage();
-            return const UniversalBlocScreen();
-            return const BlocWithAsync();
-            return const ServiceWithVlb();
-            return const VLBuildersPage();
-            return const BlocBuildWhenScreen();
-            return HomePage();
-            return BlocProvider<PageMemoryBloc>(
-              create: (final BuildContext context) => PageMemoryBloc(),
-              child: const PageMemoryScreen(),
-            );
-          },
-        ),
+        home: HomePage(),
       );
 }
